585396b07b6f0d6c41c69ec691d312fbfab20e05
Carlos <carlos@passaromx.com>
2017-09-05 14:35:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+let channels = [
+	{name: 'Hardware Support'},
+	{name: 'Software Support'}
+];
+
+class Channel extends React.Component {
+	onClick(){
+		console.log(this.props.name);
+	}
+	render(){
+		return (
+			<li onClick={this.onClick.bind(this)}>{this.props.name}</li>
+			)
+	}
+}
+
+class ChannelList extends React.Component {
+	render(){
+		return(
+			<ul>
+			{this.props.channels.map(channel => {
+					return (
+						<Channel name={channel.name} />
+					)	
+				}
+			)}
+			</ul>
+		)
+	}
+}
+
+ReactDOM.render(<ChannelList channels={channels}/>,
+document.getElementById('app'));
