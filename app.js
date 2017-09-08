a9e295d6d4fb8c91b28414475c978e3785a22c4a
Carlos Peña <carlos@passaromx.com>
2017-09-07 20:47:26 -0500

    addChannel function
    adds a channel, clean input and updates channelList as state

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-let channels = [
-	{name: 'Hardware Support'},
-	{name: 'Software Support'}
-];
+
 
 class Channel extends React.Component {
 	onClick(){
@@ -21,7 +18,7 @@ class ChannelList extends React.Component {
 			{this.props.channels.map(channel => {
 					return (
 						<Channel name={channel.name} />
-					)	
+					)
 				}
 			)}
 			</ul>
@@ -29,5 +26,62 @@ class ChannelList extends React.Component {
 	}
 }
 
-ReactDOM.render(<ChannelList channels={channels}/>,
+class ChannelForm extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {};
+	}
+	onChange(e){
+		this.setState({
+			channelName: e.target.value
+		});
+	}
+	onSubmit(e){
+		let {channelName} = this.state;
+		this.setState({
+			channelName: ''
+		});
+		this.props.addChannel(channelName);
+		e.preventDefault();
+	}
+	render(){
+		return (
+			<form onSubmit={this.onSubmit.bind(this)}>
+				<input type='text'
+					onChange={this.onChange.bind(this)}
+					value={this.state.channelName}
+				/>
+			</form>
+		)
+	}
+}
+
+class ChannelSection extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			channels: [
+				{name: 'Hardware Support'},
+				{name: 'Software Support'}
+			]
+		};
+	}
+	addChannel(name){
+		let {channels} = this.state;
+		channels.push({name: name});
+		this.setState({
+			channels: channels
+		});
+	}
+	render(){
+		return(
+			<div>
+				<ChannelList channels={this.state.channels}/>
+				<ChannelForm addChannel={this.addChannel.bind(this)}/>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<ChannelSection/>,
 document.getElementById('app'));
